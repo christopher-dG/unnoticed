5d4a14d4965fa194262e6ee7df13dbe65649b4a3
Chris de Graaf <chrisadegraaf@gmail.com>
2017-10-29 02:07:33 +0000

    Checksum binaries

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,6 +8,7 @@ if [[ $TRAVIS_PULL_REQUEST = 'false' ]] && [[ $TRAVIS_BRANCH = 'master' ]]; then
     aws configure set region us-east-1
     aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
     aws configure set aws_secret_accses_key $AWS_SECRET_ACCESS_KEY
+    md5sum $DIR/*
     aws s3 mv --recursive $DIR s3://unnoticed-deploy/$DIR
     cd - > /dev/null
 else
